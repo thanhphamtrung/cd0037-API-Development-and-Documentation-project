4149350c2e38fa2effc33eacfd0a55317590ebd4
thanhphamtrung <thanhpt22emu@gmail.com>
2023-08-12 10:29:41 +0700

    add with appcontext

--- backend/models.py
+++ backend/models.py
@@ -15,11 +15,12 @@ setup_db(app)
 
 
 def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.app = app
-    db.init_app(app)
-    db.create_all()
+    with app.app_context():
+        app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        db.app = app
+        db.init_app(app)
+        db.create_all()
 
 
 """
