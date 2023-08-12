6f1ec3b46a732ac607b0ae39e85153a1ce22c73e
thanhphamtrung <thanhpt22emu@gmail.com>
2023-08-12 10:29:30 +0700

    add api

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -78,7 +78,23 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
+    @app.route('/questions/<int:question_id>', methods=['DELETE'])
+    def delete_question(question_id):
+        try:
+            question = Question.query.get(question_id)
 
+            if question is None:
+                print('Create an endpoint to DELETE question using a question ID.')
+                abort(404, description='Question not found.')
+
+            question.delete()
+
+            return jsonify({
+                'success': True,
+                'deleted': question_id
+            })
+        except:
+            abort(422)
     """
     @TODO:
     Create an endpoint to POST a new question,
@@ -89,7 +105,36 @@ def create_app(test_config=None):
     the form will clear and the question will appear at the end of the last page
     of the questions list in the "List" tab.
     """
+    @app.route('/questions', methods=['POST'])
+    def create_question():
+        try:
+            data = request.get_json()
 
+            question_text = data.get('question', None)
+            print(question_text)
+            answer_text = data.get('answer', None)
+            category = data.get('category', None)
+            difficulty = data.get('difficulty', None)
+
+            if not (question_text and answer_text and category and difficulty):
+                abort(400, description='Missing required fields.')
+
+            question = Question(
+                question=question_text,
+                answer=answer_text,
+                category=category,
+                difficulty=difficulty
+            )
+            question.insert()
+
+            return jsonify({
+                'success': True,
+                'created': question.id
+            })
+
+        except Exception as e:
+            print(e)
+            abort(422)
     """
     @TODO:
     Create a POST endpoint to get questions based on a search term.
@@ -100,7 +145,27 @@ def create_app(test_config=None):
     only question that include that string within their question.
     Try using the word "title" to start.
     """
+    @app.route('/questions/search', methods=['POST'])
+    def search_questions():
+        try:
+            data = request.get_json()
+            search_term = data.get('searchTerm', '')
 
+            if search_term == '':
+                abort(400, description='Search term is required.')
+
+            search_results = Question.query.filter(Question.question.ilike(f'%{search_term}%')).all()
+
+            formatted_results = [question.format() for question in search_results]
+
+            return jsonify({
+                'success': True,
+                'questions': formatted_results,
+                'total_questions': len(formatted_results)
+            })
+
+        except:
+            abort(422)
     """
     @TODO:
     Create a GET endpoint to get questions based on category.
@@ -109,7 +174,26 @@ def create_app(test_config=None):
     categories in the left column will cause only questions of that
     category to be shown.
     """
+    @app.route('/categories/<int:category_id>/questions', methods=['GET'])
+    def get_questions_by_category(category_id):
+        try:
+            category = Category.query.get(category_id)
 
+            if category is None:
+                abort(404, description='Category not found.')
+
+            questions = Question.query.filter_by(category=category_id).all()
+            formatted_questions = [question.format() for question in questions]
+
+            return jsonify({
+                'success': True,
+                'questions': formatted_questions,
+                'total_questions': len(formatted_questions),
+                'current_category': category.type
+            })
+
+        except:
+            abort(422)
     """
     @TODO:
     Create a POST endpoint to get questions to play the quiz.
@@ -121,11 +205,76 @@ def create_app(test_config=None):
     one question at a time is displayed, the user is allowed to answer
     and shown whether they were correct or not.
     """
+    @app.route('/quizzes', methods=['POST'])
+    def get_quiz_question():
+        try:
+            data = request.get_json()
+            previous_questions = data.get('previous_questions', [])
+            quiz_category = data.get('quiz_category', None)
 
+            if quiz_category is None:
+                abort(400, description='Quiz category is required.')
+
+            if 'id' in quiz_category:
+                category_id = int(quiz_category['id'])
+                category = Category.query.get(category_id)
+
+                if category is None:
+                    abort(404, description='Category not found.')
+                else:
+                    questions = Question.query.filter_by(category=category_id).all()
+            else:
+                questions = Question.query.all()
+
+            remaining_questions = [question.format() for question in questions if question.id not in previous_questions]
+
+            if remaining_questions:
+                next_question = random.choice(remaining_questions)
+            else:
+                next_question = None
+
+            return jsonify({
+                'success': True,
+                'question': next_question
+            })
+
+        except:
+            abort(422)
     """
     @TODO:
     Create error handlers for all expected errors
     including 404 and 422.
     """
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': 'Bad request.'
+        }), 400
 
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'Resource not found.'
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            'success': False,
+            'error': 422,
+            'message': 'Unprocessable entity.'
+        }), 422
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return jsonify({
+            'success': False,
+            'error': 500,
+            'message': 'Internal server error.'
+        }), 500
+    
     return app
